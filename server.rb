622559a94507fc88f43b888f45adf80b5df4359c
Kmalyn <kmalyn@softserveinc.com>
2013-03-28 16:38:20 +0200

    basic rately API mock server

--- server.rb
+++ server.rb
@@ -3,62 +3,70 @@
 
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/namespace'
+require 'yaml'
 require 'json'
 
 puts "This is process #{Process.pid}"
 
 configure do
-    set :logging, true
-    set :dump_errors, true
-    set :public_folder, Proc.new { File.expand_path(File.join(root, 'Fixtures')) }
+  set :environment, :development
+  set :logging, true
+  set :dump_errors, true
+  set :public_folder, Proc.new { File.expand_path(File.join(root, 'Fixtures')) }
 end
 
-def render_fixture(filename)
-    send_file File.join(settings.public_folder, filename)
+def load_fixture(filename)
+  YAML.load_file(File.join(settings.public_folder, filename))
 end
 
-post "/1/locations/:poi_id/reviews/create.json" do
-    content_type :json
-    jdata = {:author => params[:author],
-             :text => params[:text],
-             :rating => params[:rating].to_i,
-             :id => '50f9ec0dac0cfe2db5000003',
-             :image => 'https://www.ishimr2013.com/sites/default/files/Reviews_Icon_ISHIMR_2013_v3.jpg'}.to_json
+def page
+  params[:page].nil? ? 0 : params[:page].to_i - 1 
 end
 
-get "/3/venues/:poi_id/reviews" do
-    render_fixture('ReviewMany.json')
-end
+namespace '/api' do
+  namespace '/users' do
+    
+    get '/:id/recommendations' do
+      content_type 'application/json'
+      data = load_fixture("users_recommendations.yml")
+      { 
+        :products => data['products'],
+        :categories => data['categories'][page],
+        :colors => data['colors'][page]
+      }.to_json
+    end
 
-# Creates a route that will match /3/places/exit?exitId=<exit ID>
-get '/3/places/exit' do
-    render_fixture('ExitsPoiMany.json')
-end
+    get '/:id/products/recent' do
+      content_type 'application/json'
+      load_fixture("users_products_recent.yml")[page].to_json
+    end
 
-# Creates a route that will match /3/venues/exit?exitId=<exit ID>
-get '/3/venues/exit' do
-    render_fixture('ExitsPoiMany.json')
-end
+    get '/:id/products/search' do
+      content_type 'application/json'
+      load_fixture("users_products_search.yml")[page].to_json
+    end
 
+    get '/:id/products/add' do
+      status 201
+    end
 
-# Creates a route that will match /3/venues/location?&latlng=<lat>,<long>
-get '/3/venues/location' do
-    render_fixture('ExitsPoiMany.json')
-end
+    delete '/:id/products/:product_id/delete' do
+      status 200
+    end
 
-# Creates a route that will match /articles/<category name>/<article ID>
-#get '/articles/:category/:id' do
-#    render_fixture('article.json')
-#end
+    get '/anonymous' do
+      content_type 'application/json'
+      load_fixture("users_anonymous.yml").to_json
+    end
 
-# Return a 503 response to test error conditions
-get '/offline' do
-    status 503
-end
-
-# Simulate a JSON error
-get '/error' do
-    status 400
+    get '/authenticate' do
+      content_type 'application/json'
+      load_fixture("users_authenticate.yml").to_json
+    end
+  end
+  get '/products/search' do
     content_type 'application/json'
-    "{f36a311cba6c29ba4c54f0b8c76e6cb733c01e65quot;errorf36a311cba6c29ba4c54f0b8c76e6cb733c01e65quot;: f36a311cba6c29ba4c54f0b8c76e6cb733c01e65quot;An error occurred!!f36a311cba6c29ba4c54f0b8c76e6cb733c01e65quot;}"
+    load_fixture("products_search.yml")[page].to_json
+  end
 end
